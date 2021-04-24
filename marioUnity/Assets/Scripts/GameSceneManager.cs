4c51b132877621eaa6935ce03b8c3f21ee3b491b
araigoro <kaito1019baseball@icloud.com>
2021-04-24 11:19:36 +0900

    ・ゲームオーバー画面作成 ・敵の配置、接触時にゲームオーバー画面移行 ・接地判定 ・ジャンプ導入

---
+++ marioUnity/Assets/Scripts/GameSceneManager.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameSceneManager : MonoBehaviour
+{
+    /*キャラを移動させたり、衝突のチェックをしたり、重力を加えたりするもの
+     * キャラクター操作：http://game-sakusei.com/unity/616.html
+     * https://akiblog10.com/unity-moving/
+     * ボタン操作：https://qiita.com/netty/items/66284cbb2bb1cd42a486
+     * 移動を作ろう：https://dkrevel.com/makegame-beginner/make-2d-aciton-move/
+     * 衝突判定・ジャンプ：http://game-sakusei.com/unity/628.html
+     * 接地判定：https://dkrevel.com/makegame-beginner/make-2d-action-ground/
+     */
+
+    [SerializeField] GameObject ButtonRight;
+    [SerializeField] GameObject ButtonLeft;
+    [SerializeField] GameObject ButtonJamp;
+    [SerializeField] GameObject Player;
+
+    Rigidbody2D RB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    //ここに入力する値で調節
+    public float Speed;
+    public float JampingPower;
+
+    public bool right = false;
+    public bool left = false;
+    public bool jamp = false;
+    public bool field = false;
+
+    //キーボード操作や衝突など毎回チェックがいるものはUpdate
+    // Update is called once per frame
+    void Update()
+    {
+        RB = Player.GetComponent<Rigidbody2D>();
+        if (right)
+        {
+            //Vector2(x,y)orVector3(x,y,z)の順で入れる
+            RB.velocity = new Vector2(Speed, RB.velocity.y);
+        }
+        else if (left)
+        {
+            RB.velocity = new Vector2(-Speed, RB.velocity.y);
+        }
+        else if(jamp&&field)
+        {
+            RB.velocity = new Vector2(RB.velocity.x,JampingPower);
+        }
+        else
+        {
+            RB.velocity = Vector2.zero;
+        }
+    }
+    public void RPushDown()
+    {
+        right = true;
+    }
+    public void RPushUp()
+    {
+        right = false;
+    }
+    public void LPushDown()
+    {
+        left = true;
+    }
+    public void LPushUp()
+    {
+        left = false;
+    }
+    public void JPushDown()
+    {
+        jamp = true;
+    }
+    public void FieldCheck()
+    {
+        field = true;
+    }
+
+    public void GameOver()
+    {
+        SceneManager.LoadScene("Gameover");
+    }
+}
--- marioUnity/Assets/Scripts/Player.cs
+++ marioUnity/Assets/Scripts/Player.cs
@@ -2,84 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Player : MonoBehaviour
 {
-    /*キャラを移動させたり、衝突のチェックをしたり、重力を加えたりするもの
-     * キャラクター操作：http://game-sakusei.com/unity/616.html
-     * https://akiblog10.com/unity-moving/
-     * ボタン操作：https://qiita.com/netty/items/66284cbb2bb1cd42a486
-     * 移動を作ろう：https://dkrevel.com/makegame-beginner/make-2d-aciton-move/
-     * 衝突判定・ジャンプ：http://game-sakusei.com/unity/628.html
-     * 接地判定：https://dkrevel.com/makegame-beginner/make-2d-action-ground/
-     */
-
-    public Rigidbody2D RB;
    
 
     // Start is called before the first frame update
     void Start()
     {
-        RB = GetComponent<Rigidbody2D>();
+       
     }
 
-    //ここに入力する値で調節
-    public float Speed;
-    public float JampingPower;
-
-    [SerializeField] GameObject BUttonRight;
-    [SerializeField] GameObject BUttonLeft;
-    [SerializeField] GameObject BUttonJamp;
-
-    bool right = false;
-    bool left = false;
-    bool jamp = false;
-
-    //キーボード操作や衝突など毎回チェックがいるものはUpdate
     // Update is called once per frame
     void Update()
     {
-        var Hori = Input.GetAxis("Horizontal");
-
-        if (right)
+        
+    }
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag=="Field")
         {
-            RB.velocity = new Vector2(Speed, RB.velocity.y);
+            GameObject gamemanager = GameObject.Find("GameManager");
+            gamemanager.GetComponent<GameSceneManager>().FieldCheck();
         }
-        else if (left)
+        if(collision.gameObject.tag=="Enemy")
         {
-            RB.velocity = new Vector2(-Speed, RB.velocity.y);
+            GameObject gamemanager = GameObject.Find("GameManager");
+            gamemanager.GetComponent<GameSceneManager>().GameOver();
         }
-        else if(jamp)
-        {
-            RB.AddForce(Vector2.up * JampingPower);
-        }
-        else
-        {
-            RB.velocity = Vector2.zero;
-        }
-    }
-
-    public void RPushDown()
-    {
-        right = true;
-    }
-    public void RPushUp()
-    {
-        right = false;
-    }
-    public void LPushDown()
-    {
-        left = true;
-    }
-    public void LPushUp()
-    {
-        left = false;
-    }
-    public void JPushDown()
-    {
-        jamp = true;
-    }
-    public void JPushUp()
-    {
-        jamp = false;
     }
 }
